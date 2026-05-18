37867e3cc1cb055f7e09bf272ab5a60df85ced69
jhtrujillo <jhon.murdock@gmail.com>
2026-05-18 13:57:41 -0500

    fix: corregir error de Vite/Vue templating debido al uso de v-else-if fuera de adyacencia directa

--- sivar/src/views/mejoramiento/cruzamientos/CrossingWeightedView.vue
+++ sivar/src/views/mejoramiento/cruzamientos/CrossingWeightedView.vue
@@ -108,7 +108,7 @@
       </Transition>
 
       <!-- Spinner de Carga de Ficha -->
-      <div v-else-if="isFetchingProfile" class="mt-2 mb-6 p-6 bg-slate-50/50 rounded-2xl border border-slate-100 flex items-center justify-center space-x-2">
+      <div v-if="isFetchingProfile" class="mt-2 mb-6 p-6 bg-slate-50/50 rounded-2xl border border-slate-100 flex items-center justify-center space-x-2">
         <svg class="animate-spin h-5 w-5 text-emerald-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
           <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
           <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
