4b67c75aa5cd540a2c35e7e3494a10bd93c59355
jhtrujillo <jhon.murdock@gmail.com>
2026-07-27 15:16:03 -0500

    style: ajustar grid para mostrar 4 bloques por línea en pantallas grandes

--- sivar/src/components/SuggestionMasterDetail.vue
+++ sivar/src/components/SuggestionMasterDetail.vue
@@ -66,7 +66,7 @@
         </div>
         
         <div class="flex-1 overflow-y-auto p-4 scrollbar-custom bg-slate-100/50">
-           <div class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4">
+           <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
              <div 
                 v-for="cell in sortedMales" 
                 :key="cell.varB" 
