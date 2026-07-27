7da746c2220802b15dca54171283700b01d10574
jhtrujillo <jhon.murdock@gmail.com>
2026-07-27 15:16:29 -0500

    style: ajustar grid para permitir hasta 6 columnas de tarjetas en pantallas muy grandes

--- sivar/src/components/SuggestionMasterDetail.vue
+++ sivar/src/components/SuggestionMasterDetail.vue
@@ -66,7 +66,7 @@
         </div>
         
         <div class="flex-1 overflow-y-auto p-4 scrollbar-custom bg-slate-100/50">
-           <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+           <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4">
              <div 
                 v-for="cell in sortedMales" 
                 :key="cell.varB" 
