43ea37dc6e717d4967c163bedc47b59dab0db6e7
jhtrujillo <jhon.murdock@gmail.com>
2026-09-23 10:23:40 -0500

    fix(crossings): include characters from available flowers in project characters list

--- api_sivar/app/Http/Controllers/ViveroController.php
+++ api_sivar/app/Http/Controllers/ViveroController.php
@@ -771,7 +771,22 @@ class ViveroController extends Controller
 
     public function getCaracteresPorProyecto($id)
     {
-        $caracteres = DB::table('proyecto_caracteres')->where('proyecto_id', $id)->get();
+        $fechaf = \Carbon\Carbon::today()->format('Y-m-d');
+        $fechai = \Carbon\Carbon::yesterday()->format('Y-m-d');
+
+        $caracteres = DB::table('proyecto_caracteres')
+            ->where('proyecto_id', $id)
+            ->orWhereIn('id', function($query) use ($id, $fechai, $fechaf) {
+                $query->select('id_crcter')
+                      ->from('floracion')
+                      ->where('id_pr', $id)
+                      ->where('estado', 0)
+                      ->where('bolsa_comun', 0)
+                      ->whereBetween('fcha', [$fechai, $fechaf]);
+            })
+            ->distinct()
+            ->get();
+            
         return response()->json($caracteres);
     }
 
