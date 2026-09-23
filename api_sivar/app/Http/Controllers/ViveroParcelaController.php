2bc1bd1796607f798fff9c966a71e8defcebfb6f
jhtrujillo <jhon.murdock@gmail.com>
2026-09-23 08:59:33 -0500

    fix: allow importing the same variety in multiple plots of the same vivero

--- api_sivar/app/Http/Controllers/ViveroParcelaController.php
+++ api_sivar/app/Http/Controllers/ViveroParcelaController.php
@@ -111,15 +111,6 @@ class ViveroParcelaController extends Controller
                     }
                 }
 
-                if ($data['variedad_id'] !== null && $data['variedad_id'] !== '') {
-                    \Log::info("Checking variedad_id: " . $data['variedad_id'] . " for plot: " . $data['numero_parcela']);
-                    $existsVariedad = $vivero->parcelas()
-                        ->where('variedad_id', $data['variedad_id'])
-                        ->where('numero_parcela', '!=', $data['numero_parcela'])
-                        ->exists();
-                    if ($existsVariedad) continue;
-                }
-
                 \Log::info("Updating plot: " . $data['numero_parcela'] . " with variedad_id: " . $data['variedad_id']);
 
                 $parcela = $vivero->parcelas()->updateOrCreate(
